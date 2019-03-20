baa979464d6710ee020b0828ac0dd8e3858385c2
nkaccounting <759893882@qq.com>
2019-03-20 20:57:42 +0800

    hw3 2019/3/20

--- homework3/src/printPrime/testPrimes.java
+++ homework3/src/printPrime/testPrimes.java
@@ -14,7 +14,8 @@ import org.junit.Test;
 
 public class testprimes {
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
+	
+	//console信息转化到string中
 	@Before
 	public void setUpStreams() {
 	    System.setOut(new PrintStream(outContent));
@@ -27,20 +28,24 @@ public class testprimes {
 	
 	
 	@Test
-	public void testmoney() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+	public void testp() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
 		PrintPrimes pp=new PrintPrimes();
 		//由于是private方法，采用反射机制，用method的方法调用函数
 		Class pc=pp.getClass();
 		Method m=pc.getDeclaredMethod("printPrimes", new Class[]{int.class});
 		m.setAccessible(true);
-		m.invoke(pp, 5);
-
+		m.invoke(pp, 5);//n=5
+		
+		m.invoke(pp, 1);//n=1
+		
 		String expect="Prime: 2"+'\r'+'\n';
 		expect=expect+"Prime: 3"+'\r'+'\n';
 		expect=expect+"Prime: 5"+'\r'+'\n';
 		expect=expect+"Prime: 7"+'\r'+'\n';
-		expect=expect+"Prime: 11"+'\r'+'\n';
+		expect=expect+"Prime: 11"+'\r'+'\n';//n=5
 		
+		expect=expect+"Prime: 2"+'\r'+'\n';//n=1
+
 		assertEquals(expect,outContent.toString());
 	}
 }
